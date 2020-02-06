ce500c55563328fdb8f29c1b4202f50413bbade3
cbpark <cbpark@gmail.com>
2020-02-06 16:04:21 +0900

    Add more functions for couplings

--- src/HEP/Data/Kinematics.hs
+++ src/HEP/Data/Kinematics.hs
@@ -9,3 +9,10 @@ massSq (Mass m) = m * m
 
 massRatio :: Mass -> Mass -> Double
 massRatio (Mass m1) (Mass m2) = m1 / m2
+
+betaF :: Mass  -- ^ heavy particle
+      -> Mass  -- ^ light particle
+      -> Double
+betaF mY mF = if a' < b then 0 else sqrt (1 - b / a)
+  where (a, b) = (massSq mY, 4 * massSq mF)
+        a' = a + 1.0e-8
--- src/HEP/Data/THDM/Coupling.hs
+++ src/HEP/Data/THDM/Coupling.hs
@@ -1,12 +1,12 @@
 module HEP.Data.THDM.Coupling where
 
-import HEP.Data.Constants  (gW, mW)
+import HEP.Data.Constants  (gW, mW, mtau)
 import HEP.Data.Kinematics (Mass (..), massRatio)
 import HEP.Data.Util       (sinBetaAlpha)
 
 data THDMType = TypeI | TypeII | UnknownType deriving Eq
 
-gHff :: (Double -> Double)  -- ^ function to get the coefficient
+gHff :: (Double -> Double)  -- ^ a function to get the coefficient
      -> Double              -- ^ tan(beta)
      -> Double              -- ^ cos(beta - alpha)
      -> Mass                -- ^ fermion mass
@@ -24,3 +24,20 @@ gHUU typ mU tanb cosba = gHff cfunc tanb cosba mU
   where
     cfunc sinba | typ == TypeI || typ == TypeII = cosba - sinba / tanb
                 | otherwise                     = 0
+
+gHDD :: THDMType
+     -> Mass    -- ^ fermion mass
+     -> Double  -- ^ tan(beta)
+     -> Double  -- ^ cos(beta - alpha)
+     -> Double
+gHDD typ mD tanb cosba = gHff cfunc tanb cosba mD
+  where
+    cfunc sinba | typ == TypeI  = cosba - sinba / tanb
+                | typ == TypeII = cosba + sinba * tanb
+                | otherwise     = 0
+
+gHTauTauI, gHTauTauII :: Double  -- ^ tan(beta)
+                      -> Double  -- ^ cos(beta - alpha)
+                      -> Double
+gHTauTauI  = gHDD TypeI mtau
+gHTauTauII = gHDD TypeII mtau
