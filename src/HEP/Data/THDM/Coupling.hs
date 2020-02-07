5ab0f7bee385a27718959d2c32242bf7a691da36
cbpark <cbpark@gmail.com>
2020-02-07 17:04:57 +0900

    Add the gHHpHm coupling

--- src/HEP/Data/THDM/Coupling.hs
+++ src/HEP/Data/THDM/Coupling.hs
@@ -1,7 +1,7 @@
 module HEP.Data.THDM.Coupling where
 
 import HEP.Data.Constants  (gW, mW, mtau)
-import HEP.Data.Kinematics (Mass (..), massRatio)
+import HEP.Data.Kinematics (Mass (..), massRatio, massSq)
 import HEP.Data.Util       (sinBetaAlpha)
 
 data THDMType = TypeI | TypeII | UnknownType deriving (Eq, Show)
@@ -37,3 +37,17 @@ gHTauTauI, gHTauTauII :: (Double, Double)  -- ^ ((tan(beta), cos(beta - alpha)))
                       -> Double
 gHTauTauI  = gHDD TypeI mtau
 gHTauTauII = gHDD TypeII mtau
+
+gHHpHm :: Mass              -- ^ m_H
+       -> Mass              -- ^ m_A
+       -> Mass              -- ^ m_{H+}
+       -> (Double, Double)  -- ^ (tan(beta), cos(beta - alpha))
+       -> Double
+gHHpHm mH mA mHp (tanb, cosba) =
+    let mH2  = massSq mH
+        mA2  = massSq mA
+        mHp2 = massSq mHp
+        sinba = sinBetaAlpha tanb cosba
+        tan2b = 2 * tanb / (1 - tanb * tanb)
+        c = gW / (2 * getMass mW)
+    in c * (cosba * (mH2 + 2 * (mHp2 - mA2)) - 2 * sinba / tan2b * (mH2 - mA2))
